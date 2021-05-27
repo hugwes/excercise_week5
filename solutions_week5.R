f47f4f92611454ccfdf600f7ab51a69e72943b0b
Wesley Hug <hugwes@students.zhaw.ch>
2021-05-27 11:37:21 +0200

    starting task 3

--- solutions_week5.R
+++ solutions_week5.R
@@ -20,7 +20,23 @@ wildschwein <- read_delim("wildschwein_BE_2056.csv",",") %>%
   st_as_sf(coords = c("E", "N"), crs = 2056, remove = FALSE)
 
 ##########################################################
-# Task 1:
+# Task 1: Import and visualize spatial data
+feldaufnahmen <- read_sf("Feldaufnahmen_Fanel.gpkg") 
+
+# this is a vector-dataset stored in the filetype Geopackage (similar to shapefile)
+# there are 975 polygons with fruit types
+# there are 44 different fruit-types (levels)
+
+##########################################################
+# Task 2: Annotate Trajectories from vector data
+
+# Filter the months may to june
+wildschwein_filter <- wildschwein %>%
+  mutate(Monat = month(DatetimeUTC)) %>%
+  filter(Monat == 5 | Monat == 6)
+
+# Overlap wildschwein_filter with feldaufnahmen
+
 
 
 
