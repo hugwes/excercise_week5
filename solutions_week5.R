6144f341133cdd656c829bf507eec7ba5bc2997a
Wesley Hug <hugwes@students.zhaw.ch>
2021-05-27 11:08:17 +0200

    starting excercise 5

---
+++ solutions_week5.R
@@ -0,0 +1,29 @@
+
+### EXCERCISE 5 ###
+
+##########################################################
+# Preparations
+# https://github.com/hugwes/excercise_week5 (github-link)
+
+##########################################################
+# Load in Libraries
+library(readr)        # to import tabular data (e.g. csv)
+library(dplyr)        # to manipulate (tabular) data
+library(ggplot2)      # to visualize data
+library(sf)           # to handle spatial vector data (sf=shape file)
+library(terra)        # to handle raster data
+library(lubridate)    # to handle dates and times
+library(zoo)          # moving window functions
+
+# Import data
+wildschwein <- read_delim("wildschwein_BE_2056.csv",",")
+
+# Convert into sf-object (shape file)
+wildschwein <- st_as_sf(wildschwein, coords=c("E","N"), crs=2056, remove=FALSE)
+
+##########################################################
+# Task 1:
+
+
+
+
